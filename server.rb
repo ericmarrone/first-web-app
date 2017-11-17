17dda309824218da530428a2e69bb45d687b8feb
Eric Marrone <emarrone17@gmail.com>
2017-11-16 21:02:41 -0500

    Add favourites page with fav links

--- server.rb
+++ server.rb
@@ -9,5 +9,12 @@ get "/portfolio" do
 end
 
 get "/about_me" do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about
+end
+
+get "/favourites" do
+  @fav_links = [{ 'Google' => 'http://www.google.com'}, { 'Amazon'=> 'http://www.amazon.com'}]
+  erb :favourites
 end
